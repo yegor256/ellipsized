fe938288846e6d8af42a2011c780caf48d5505f4
Yegor Bugayenko <yegor256@gmail.com>
2025-05-21 08:02:43 +0300

    #3 claude docs

--- lib/ellipsized.rb
+++ lib/ellipsized.rb
@@ -9,8 +9,32 @@
 # Copyright:: Copyright (c) 2025 Yegor Bugayenko
 # License:: MIT
 class String
-  # @param [Integer] max The maximum length of the string, expected
-  # @return [String] The text with ellipsis inside (if necessary)
+  # Truncates a string to specified maximum length, inserting ellipsis in the middle
+  # if necessary. The resulting string will never be longer than the specified
+  # maximum length. The original string is returned if it is already shorter than
+  # or equal to the maximum length.
+  #
+  # @param [Integer] max The maximum length of the resulting string
+  # @param [String] ellipsis The string to use as ellipsis (default: '...')
+  # @return [String] The truncated string with ellipsis in the middle if necessary
+  #
+  # @example Basic usage with default parameters
+  #   "Hello, world!".ellipsized
+  #   # => "Hello, world!"
+  #
+  # @example Truncate a long string
+  #   "This is a very long string that needs to be truncated".ellipsized(20)
+  #   # => "This is...truncated"
+  #
+  # @example Custom ellipsis
+  #   "Another very long string example".ellipsized(15, ellipsis: "***")
+  #   # => "Anoth***example"
+  #
+  # @example Edge cases
+  #   "".ellipsized      # => ""
+  #   "Short".ellipsized # => "Short"
+  #   "xyz".ellipsized(0) # => ""
+  #   "xyz".ellipsized(2, ellipsis: "...") # => "xy"
   def ellipsized(max = 64, ellipsis: '...')
     return '' if empty?
     return self if length <= max
